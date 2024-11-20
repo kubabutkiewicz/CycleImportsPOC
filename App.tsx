e15dbf52a27ffe3156615b828a2a47e5417ff76c
kubabutkiewicz <jakub.butkiewicz94@gmail.com>
2024-11-20 13:07:39 +0100

    .

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import * as ServiceA from './libs/ServiceA';
-import * as ServiceB from './libs/ServiceB';
-import * as ServiceC from './libs/ServiceC';
+import {useServiceB} from './libs/ServiceA';
+import {useServiceC} from './libs/ServiceB';
+import {useServiceA} from './libs/ServiceC';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>React Native Web App</Text>
-      <Text>{ServiceA.doSomethingA()}</Text>
-      <Text>{ServiceB.doSomethingB()}</Text>
-      <Text>{ServiceC.doSomethingC()}</Text>
+      <Text>{useServiceB()}</Text>
+      <Text>{useServiceC()}</Text>
+      <Text>{useServiceA()}</Text>
     </View>
   );
 }
